dbc0910ab34e26d471fa2bbfc00fca5186c7bb5a
DIEGO FARIAS GOMES <diegopaladinofoto@gmail.com>
2024-02-15 17:42:18 -0300

    AUTOprint_150220241742

---
+++ Nova pasta/FICHA_PNLD.py
@@ -0,0 +1,122 @@
+import os
+import json
+import pyautogui as p
+import time as t
+import clipboard as c
+import pyperclip as pp
+import tkinter as tk #recurso para criação de interface IHM
+from tkinter import simpledialog
+
+
+
+
+def abrir_nova_aba():
+    p.keyDown('ctrl')
+    t.sleep(0.2)
+    p.hotkey('t')
+    t.sleep(0.3)
+    p.keyUp('ctrl')
+    t.sleep(0.2)
+def ctrl_pageup():
+    p.keyDown('ctrl')
+    t.sleep(0.2)
+    p.hotkey('pageup')
+    t.sleep(0.3)
+    p.keyUp('ctrl')
+    t.sleep(0.2)
+def ctrl_pagedown():
+    p.keyDown('ctrl')
+    t.sleep(0.2)
+    p.hotkey('pagedown')
+    t.sleep(0.3)
+    p.keyUp('ctrl')
+    t.sleep(0.2)
+def pageup():
+    p.hotkey('pageup')
+    t.sleep(0.3)
+def pagedown():
+    p.hotkey('pagedown')
+    t.sleep(0.3)
+def procurar():
+    p.keyDown('ctrl')
+    t.sleep(0.2)
+    p.hotkey('f')
+    t.sleep(0.3)
+    p.keyUp('ctrl')
+    t.sleep(0.2)
+def copiar():
+    p.keyDown('ctrl')
+    t.sleep(0.2)
+    p.hotkey('c')
+    t.sleep(0.3)
+    p.keyUp('ctrl')
+    t.sleep(0.2)
+def colar():
+    p.keyDown('ctrl')
+    t.sleep(0.2)
+    p.hotkey('v')
+    t.sleep(0.3)
+    p.keyUp('ctrl')
+    t.sleep(0.2)
+def enter():
+    p.press('enter')
+    t.sleep(0.3)
+def tab():
+    p.press('tab')
+    t.sleep(0.3)
+def alt_tab():
+    p.keyDown('alt')
+    t.sleep(0.2)
+    p.hotkey('tab')
+    t.sleep(0.3)
+    p.keyUp('alt')
+    t.sleep(0.2)
+def imprimir():
+    p.keyDown('ctrl')
+    t.sleep(0.2)
+    p.hotkey('p')
+    t.sleep(0.3)
+    p.keyUp('ctrl')
+    t.sleep(0.2)
+def enter():
+    p.keyDown('enter')
+    t.sleep(0.2)
+    p.keyUp('enter')
+    t.sleep(0.2)
+def down():
+    p.keyDown('down')
+    t.sleep(0.2)
+    p.keyUp('down')
+    t.sleep(0.2)
+
+#alternando para a janela excel
+alt_tab()
+
+while True:
+    
+    #alternar para janela excel onde está a lista
+    ctrl_pagedown()
+    print('pagedown')
+    
+    #mudando para a célula de baixo
+    down()
+    print('down')
+
+    #copiando o número da matrícula
+    copiar()
+    print('copiar')
+
+    #alternar para janela excel onde está a FICHA
+    ctrl_pageup()
+    print('pageup')
+
+    #colando número matrícula
+    colar()
+    print('colar')
+
+    #imprimir
+    imprimir()
+    print('imprimir')
+    enter()
+    print('enter')
+    t.sleep(2)
