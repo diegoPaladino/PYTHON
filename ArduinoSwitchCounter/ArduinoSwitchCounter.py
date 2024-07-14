744ea090b0151a340e45ea6ec4b8c4a11de17d0d
DIEGO FARIAS GOMES <diegopaladinofoto@gmail.com>
2024-07-14 20:30:36 -0300

    Atualizar o ArduinoSwitchCounter.py

--- ArduinoSwitchCounter/ArduinoSwitchCounter.py
+++ ArduinoSwitchCounter/ArduinoSwitchCounter.py
@@ -31,4 +31,4 @@ if __name__ == "__main__":
         
 # code to implementation in a secretary's door
 # starting work in August. preparate in holidays. 
-# implement at work begin
+# implement at work begin the next month
